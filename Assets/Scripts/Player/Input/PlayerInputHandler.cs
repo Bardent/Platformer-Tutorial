baccbd0eaed193dd681161d946bacca57ea6c54e
Bardent <37575819+Bardent@users.noreply.github.com>
2020-07-26 09:46:50 +0200

    Part 20

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private Vector2 movementInput;
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+        Debug.Log(movementInput);
+    }
+
+    public void OnJumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            Debug.Log("Jump button pushed down now");
+        }
+
+        if (context.performed)
+        {
+            Debug.Log("Jump is being held down");
+        }
+
+        if (context.canceled)
+        {
+            Debug.Log("Jump button has been released");
+        }
+
+      
+    }
+}
