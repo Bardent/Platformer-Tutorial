7656c7f0fbcf5dae08f4b4e2610b735df8e8b774
Heinrich Blom <bardentstudios@gmail.com>
2021-09-30 19:37:37 +0200

    Part 34 Done For Medium

---
+++ Assets/Scripts/Core/CoreComponents/Stats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Stats : CoreComponent
+{
+    [SerializeField] private float maxHealth;
+    private float currentHealth;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        currentHealth = maxHealth;
+    }
+
+    public void DecreaseHealth(float amount)
+    {
+        currentHealth -= amount;
+
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Health is zero!!");
+        }
+    }
+
+    public void IncreaseHealth(float amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+    }
+}
