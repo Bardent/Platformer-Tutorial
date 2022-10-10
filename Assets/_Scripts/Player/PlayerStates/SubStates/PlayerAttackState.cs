b3d342d51bb46fbd8d742654dd1189d0f40597ca
Heinrich <37575819+Bardent@users.noreply.github.com>
2022-10-10 12:33:02 -0500

    Part 37 - YouTube Tutorial
    Project clean up to get ready for Weapon System Tutorial Series

---
+++ Assets/_Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackState : PlayerAbilityState
+{
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName)
+        : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+}
